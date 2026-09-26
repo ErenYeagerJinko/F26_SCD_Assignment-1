45b4e550ae920bc2221097b8d7093216985c3314
M. Babar Shahzad <babarshahzad2005@gmail.com>
2026-09-26 11:52:11 +0500

    Enable request lists to be ordered by priority and date

--- src/comparators/RequestDateComparator.java
+++ src/comparators/RequestDateComparator.java
@@ -1,10 +1,20 @@
 package comparators;
 
 import java.util.Comparator;
+import model.Request;
 
-class RequestDateComparator implements Comparator<Request> {
+public class RequestDateComparator implements Comparator<Request> {
     @Override
     public int compare(Request r1, Request r2) {
+        if (r1 == null && r2 == null) {
+            return 0;
+        }
+        if (r1 == null) {
+            return 1;
+        }
+        if (r2 == null) {
+            return -1;
+        }
         return r1.getRequestDate().compareTo(r2.getRequestDate());
     }
 }
